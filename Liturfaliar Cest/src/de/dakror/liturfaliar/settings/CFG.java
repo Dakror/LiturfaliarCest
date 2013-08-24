2c17c5f23ebe48f6441e5ff5144e0ffacb8b1b39
Dakror <mail@dakror.de>
2013-08-24 15:36:38 +0200

    Update

--- Liturfaliar Cest/src/de/dakror/liturfaliar/settings/CFG.java
+++ Liturfaliar Cest/src/de/dakror/liturfaliar/settings/CFG.java
@@ -14,7 +14,7 @@ import de.dakror.liturfaliar.util.FileManager;
 public final class CFG
 {
   // -- UniVersion -- //
-  public static final int      VERSION             = 2013072215;
+  public static final int      VERSION             = 2013082415;
   public static final int      PHASE               = 0;
   // -- debug -- //
   public static boolean        UIDEBUG             = false;
