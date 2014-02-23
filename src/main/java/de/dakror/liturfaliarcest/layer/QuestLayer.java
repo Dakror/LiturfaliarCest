e636bd0b7d419bbc854ca0f63f627f1e9d00961b
Dakror <mail@dakror.de>
2014-02-23 20:48:03 +0100

    questlayer

---
+++ src/main/java/de/dakror/liturfaliarcest/layer/QuestLayer.java
@@ -0,0 +1,23 @@
+package de.dakror.liturfaliarcest.layer;
+
+import java.awt.Graphics2D;
+
+import de.dakror.gamesetup.layer.Layer;
+
+/**
+ * @author Dakror
+ */
+public class QuestLayer extends Layer
+{
+	@Override
+	public void draw(Graphics2D g)
+	{}
+	
+	@Override
+	public void update(int tick)
+	{}
+	
+	@Override
+	public void init()
+	{}
+}
